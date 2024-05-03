51732205c3787e9757e59bff01da29d78e8bad73
umpu <carol.ledgers0h@icloud.com>
2024-05-03 17:38:22 +0500

    Initial Commit

---
+++ LazyTodo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LazyTodo
+//
+//  Created by Sonoma on 5/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LazyTodo/LazyTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyTodoApp.swift
+//  LazyTodo
+//
+//  Created by Sonoma on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LazyTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
